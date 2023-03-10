891f1fee378db3f12a092f1da7d9588ed07eaf59
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-03-10 21:14:10 +0100

    Clean up listen callback

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import "./setup";
 import Koa from "koa";
 import router from "./routes";
 
-import client from "./clients/discord";
 import "./commands";
 
 const app = new Koa();
@@ -11,10 +10,4 @@ const app = new Koa();
 app
     .use(router.routes())
     .use(router.allowedMethods())
-    .listen(3000, async () => {
-        // console.log("Saving commands");
-        // await client.saveCommands();
-        // console.log("Done saving commands");
-
-        console.log("Server started on port 3000 (http://localhost:3000)");
-    });
+    .listen(3000, async () => console.log("Server started on port 3000 (http://localhost:3000)"));
