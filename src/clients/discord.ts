aed3682de6d5aa76726959b1a2c0ec42cb3e4419
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-03-17 14:29:55 +0100

    Revert "Remove unused discord command logic"
    This reverts commit 03395fbdab93e8fb8dc11735b6013a537e99864b.

--- src/clients/discord.ts
+++ src/clients/discord.ts
@@ -1,6 +1,14 @@
-import { Client } from "discord.js";
+import { Client, Events, SlashCommandBuilder, Interaction, CommandInteraction, REST, Routes, InteractionType, ApplicationCommandType } from "discord.js";
+
+export interface Command {
+    info: SlashCommandBuilder,
+    execute: (interaction: CommandInteraction) => Promise<void>;
+}
 
 export interface IClient extends Client {
+    commands: Map<string, Command>;
+    addCommand: (command: Command) => void;
+    saveCommands: () => Promise<void>;
     /*
     * Waits for client to be ready, if it is already ready it will immidiately resolve
     */
@@ -11,8 +19,40 @@ const client = new Client({
     intents: ["DirectMessages"],
 }) as IClient;
 
+const rest = new REST({ version: "10" });
+
+client.commands = new Map<string, Command>();
+client.addCommand = (command: Command) => {
+    client.commands.set(command.info.name, command);
+};
+client.saveCommands = async () => {
+    const commands = [];
+    for (const [_, command] of client.commands.entries()) {
+        commands.push(command.info.toJSON());
+    }
+    await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID!, process.env.DISCORD_GUILD_ID!), {
+        body: commands
+    });
+};
+
+rest.setToken(process.env.DISCORD_TOKEN!);
 client.login(process.env.DISCORD_TOKEN);
 client.user?.setStatus("online");
+
+client.on("interactionCreateHook", (interaction: Interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) {
+        interaction.reply({
+            content: "Command does not exist",
+            ephemeral: true
+        });
+        return;
+    };
+
+    command.execute(interaction);
+});
 
 client.awaitReady = async function () {
     const isReady = this.isReady();
