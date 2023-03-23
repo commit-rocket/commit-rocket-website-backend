b6f45d2323f0d35c641f5d5794354e25921d58b4
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-03-23 19:49:37 +0100

    Package the dist differently for easier deployment

--- scripts/package.ts
+++ scripts/package.ts
@@ -1,6 +1,5 @@
 import fs from "fs/promises";
 import path from "path";
-import { cwd } from "process";
 
 interface PackageJson {
     name?: string;
@@ -10,23 +9,44 @@ interface PackageJson {
     engines?: Record<string, string>;
 }
 
-const run = async () => {
-    const mainPackagePath = path.resolve(__dirname, "../package.json");
-    const distPackagePath = path.join(__dirname, "../dist/package.json");
-
+const makePackageJson = async (mainPackagePath: string, packagePath: string) => {
     const fileContent = (await fs.readFile(mainPackagePath)).toString();
     const { name, dependencies, devDependencies, engines } = JSON.parse(fileContent) as PackageJson;
 
-    await fs.writeFile(distPackagePath, JSON.stringify({
+    await fs.writeFile(packagePath, JSON.stringify({
         name,
         dependencies,
         devDependencies,
         engines,
         "scripts": {
-            "start": "node ./index.js"
+            "start": "node ./dist/index.js"
         }
     } as PackageJson));
-
 };
 
-run().then(() => console.log("\"./dist\" folder is ready"));
+const copyDist = async (distPath: string, packagedDistPath: string) => {
+    await fs.cp(distPath, packagedDistPath, {
+        recursive: true
+    });
+};
+
+const run = async () => {
+    const mainPackagePath = path.resolve(__dirname, "../package.json");
+    const outputDestination = path.resolve(__dirname, "../built-package");
+    const distPath = path.resolve(__dirname, "../dist");
+
+    const packagePath = path.join(outputDestination, "/package.json");
+    const packagedDistPath = path.join(outputDestination, "/dist");
+
+    await fs.rm(outputDestination, {
+        recursive: true,
+        force: true
+    });
+
+    await Promise.all([
+        makePackageJson(mainPackagePath, packagePath),
+        copyDist(distPath, packagedDistPath)
+    ]);
+};
+
+run().then(() => console.log("\"./built-package\" folder is ready"));
